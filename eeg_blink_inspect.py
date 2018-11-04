795b20831f6b21ce0297d64ce4a2b7d7c0e9696c
Yaman Özakın <dandik@gmail.com>
2018-11-04 22:22:22 +0300

    reject events with blinks

--- eeg_blink_inspect.py
+++ eeg_blink_inspect.py
@@ -5,7 +5,7 @@ import mne
 
 # Load the file and add a virtual channel for re-referencing:
 
-raw=mne.io.read_raw_brainvision('Obj0001.vhdr',preload=True)
+raw=mne.io.read_raw_brainvision('Obj0002.vhdr',preload=True)
 mne.add_reference_channels(raw, 'LiRef', copy=False)
 raw.set_eeg_reference(['ReRef','LiRef'])
 
@@ -28,6 +28,15 @@ tmin, tmax = -0.2, 0.5
 baseline = (None, 0.0)
 raw.set_channel_types({'EOGli':'eog','EOGre':'eog','EOGobre':'eog','EOGunre':'eog'})
 reject = {'eog': 40e-6}
+
+eog_events = mne.preprocessing.find_eog_events(raw)
+n_blinks = len(eog_events)
+onset = eog_events[:, 0] / raw.info['sfreq'] - 0.25
+duration = np.repeat(0.5, n_blinks)
+raw.annotations = mne.Annotations(onset, duration, ['bad blink'] * n_blinks,
+                                  orig_time=raw.info['meas_date'])
+print(raw.annotations)  # to get information about what annotations we have
+raw.plot(events=eog_events)  # To see the annotated segments.
 
 picks = mne.pick_types(raw.info,eeg=True,eog=True)
 events = mne.find_events(raw)
@@ -58,7 +67,9 @@ epochs = mne.Epochs(raw,
                     tmax=tmax, 
                     baseline=None, 
                     detrend=0,
-                    picks=picks)
+                    picks=picks,
+                    #reject=reject,
+                    reject_by_annotation=True)
 
 #epochs.drop_bad()
 #epochs.plot_drop_log()
